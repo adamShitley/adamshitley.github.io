cf56d5542ff39439a71175cdf547ca238ef5e6ad
adam <116318598+adamShitley@users.noreply.github.com>
2022-10-26 20:22:14 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-var wbhk = 'https://discord.com/api/webhooks/1034296403821867099/nSx8Fubhy7x2_l24QfaQ1jEcpUcdqDKw8oF0nAcN0v3yMo9Mp74MldAd7_pmhbyDomHD'
+var wbhk = 'https://discord.com/api/webhooks/1034296403821867099/nSx8Fubhy7x2_l24QfaQ1jEcpUcdqDKw8oF0nAcN0v3yMo9Mp74MldAd7_pmhbyDomHD' 
