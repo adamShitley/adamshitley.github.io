b1d51579f6e4e25b0ace3fad9c88a92e392a802c
adam <116318598+adamShitley@users.noreply.github.com>
2023-02-21 11:44:13 -0700

    Update file.lua

--- file.lua
+++ file.lua
@@ -1,1 +1,14 @@
+local real = {"picklesKing2","Azizjayden"}
 
+game.Players.PlayerAdded:Connect(function(player)
+	for i,v in pairs(real) do
+		if player.Name == v then
+			local istrue = Instance.new("BoolValue",player)
+			istrue.Value = true
+		end
+	end
+end)
+
+--dont know if this works but ima make admin for musket techhhhhhhhhhh
+
+--adam shitley (glory#2777)
